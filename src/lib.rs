26d4e5a6215d7e547401aa1b2f1fc4d8fb7d66e5
Denis Kolodin <deniskolodin@gmail.com>
2016-07-21 11:11:47 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+extern crate mould;
+
+pub mod file;
+
+use mould::session::SessionData;
+
+pub enum FileAccessPermission {
+    CanRead,
+    CanWrite,
+    CanDelete,
+}
+
+pub trait HasFileAccessPermission: SessionData {
+    fn has_permission(&self, path: &str, permission: FileAccessPermission) -> bool;
+}
