aef07c9a05d5f07de4ca46b5b3ad3640f970ee68
Denis Kolodin <deniskolodin@gmail.com>
2016-07-26 16:38:18 +0300

    Implement file reading

--- src/file.rs
+++ src/file.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::prelude::*;
 use mould::prelude::*;
 use super::{FileAccessPermission, HasFileAccessPermission};
 
@@ -11,9 +13,10 @@ impl FileHandler {
 
 impl<CTX> Handler<CTX> for FileHandler where CTX: HasFileAccessPermission {
     fn build(&self, mut request: Request) -> Box<Worker<CTX>> {
-        if request.action == "read" {
+        if request.action == "read-file" {
             Box::new(FileReadWorker {
                 name: request.extract("name"),
+                file: None,
             })
         } else {
             let msg = format!("Unknown action '{}' for file service!", request.action);
@@ -24,16 +27,26 @@ impl<CTX> Handler<CTX> for FileHandler where CTX: HasFileAccessPermission {
 
 struct FileReadWorker {
     name: Option<String>,
+    file: Option<File>,
 }
 
 impl<CTX> Worker<CTX> for FileReadWorker where CTX: HasFileAccessPermission {
     fn shortcut(&mut self, session: &mut CTX) -> WorkerResult<Shortcut> {
         let name = try!(self.name.take().ok_or(
-                WorkerError::Reject("Name of file not set.".to_string())));
+            WorkerError::Reject("Name of file not set.".to_string())));
         if session.has_permission(&name, FileAccessPermission::CanRead) {
+            let file = try!(File::open(&name));
+            self.file = Some(file);
             Ok(Shortcut::Tuned)
         } else {
             Err(WorkerError::Reject("You haven't permissions!".to_string()))
         }
+    }
+    fn realize(&mut self, _: &mut CTX, _: Option<Request>) -> WorkerResult<Realize> {
+        let mut file = try!(self.file.take().ok_or(
+            WorkerError::reject("File handle was lost.")));
+        let mut content = String::new();
+        try!(file.read_to_string(&mut content));
+        Ok(Realize::OneItemAndDone(mould_object!{"content" => content}))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate mould;
 
 pub mod file;
